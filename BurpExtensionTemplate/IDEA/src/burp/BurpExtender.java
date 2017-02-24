af383af1975ea91dce45f5669e0fad3af4f8c7f6
Luca Carettoni <luca@doyensec.com>
2017-02-24 21:36:43 +0100

    IDEA template

---
+++ BurpExtensionTemplate/IDEA/src/burp/BurpExtender.java
@@ -0,0 +1,23 @@
+/*
+ * This is an empty Burp Extension - Add here your file header
+ */
+
+package burp;
+
+//Depending on the extension type, you'll add additional 'implements' interfaces here
+public class BurpExtender implements IBurpExtender
+{
+
+    private IBurpExtenderCallbacks callbacks;
+    private IExtensionHelpers helpers;
+
+    @Override
+    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks)
+    {
+        this.callbacks = callbacks;
+        helpers = callbacks.getHelpers();
+        callbacks.setExtensionName("Empty Burp Extension");
+
+        //Your code goes here!
+    }
+}
